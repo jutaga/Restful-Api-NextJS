302e11f177ded3bbb4d398dfdc052940030594c8
Juan Pablo Tabares Gallego <juancho9615@hotmail.com>
2025-01-16 11:23:51 -0500

    Config next auth with github

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,6 +9,8 @@ import {
   IoListOutline,
 } from "react-icons/io5";
 import { SidebarItem } from "../sidebarItem/SidebarItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const menuItems = [
   {
@@ -38,7 +40,15 @@ const menuItems = [
   }
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async() => {
+
+  const session = await getServerSession(authOptions) ;
+
+  const avatarURL = (session?.user?.image)  ? session.user.image : "/images/image.png" ;
+  const username = session?.user?.name || "No name";
+ //const userRole = session?.user?.name || "No name";
+
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -47,7 +57,7 @@ export const Sidebar = () => {
           <Link href="/dashboard" title="Dashboard">
             {/* Next/Image */}
             <Image
-              src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"
+              src="/images/next.svg"
               className="w-32"
               alt="tailus logo"
               width={150}
@@ -60,7 +70,7 @@ export const Sidebar = () => {
         <div className="mt-8 text-center">
           {/* Next/Image */}
           <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src={avatarURL}
             alt=""
             width={150}
             height={150}
@@ -68,7 +78,7 @@ export const Sidebar = () => {
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            {username}
           </h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
